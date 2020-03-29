1ab3d2404c406f70634778bef0d0c79ae74ff65b
iamkelen <gkalashnick@gmail.com>
2020-03-29 16:18:47 +0300

    add date validation to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,10 @@
 class Task < ApplicationRecord
   belongs_to :project
   validates :title, presence: true, length: { maximum: 255 }
-  validates :date_cannot_be_in_the_past, presence: true
+  validates :date, presence: true
+  validate :future_date
 
-  def date_cannot_be_in_the_past
+  def future_date
     if date.present? && date < Date.today
       errors.add(:date, "can't be in the past")
     end
